3884b7d7846e0834487f405e514f703cf2792ff9
Caitlin Brennan <c00282149@setu.ie>
2025-02-07 16:44:23 +0000

    socials added

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+function openGithub(){
+    window.open("https://github.com/caitlinbrennan");
+}
+function openDiscord(){
+    window.open("https://www.discordapp.com/users/811303542475456542");
+}
+function openLinkedIn(){
+    window.open("https://linkedin.com/in/caitlin-brennan-1bb736304/");
+}
