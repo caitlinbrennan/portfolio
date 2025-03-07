027a28093d0abd8a8ca9c2a4103176c043c2f5de
Caitlin Brennan <c00282149@setu.ie>
2025-03-07 15:07:08 +0000

    links edited

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,3 +20,13 @@ function myFunction() {
       x.style.display = "block";
     }
   }
+
+function openGameJam(){
+  window.open("https://globalgamejam.org/games/2025/i-dont-want-burst-your-bubble-3");
+}
+function openHeadphone(){
+  window.open("file:///C:/Users/idaduser/OneDrive%20-%20South%20East%20Technological%20University/Year%202/Semester%202/Web%20User%20Interface%20Design/Web-User-Interface-Design/Practicals/Lab%205/index.html");
+}
+function openGame(){
+  window.open("file:///C:/Users/idaduser/OneDrive%20-%20South%20East%20Technological%20University/Year%202/Semester%201/UI/Programming/Caitlin_Brennan_C00282149-UI-Programming-Module-Project/index.html");
+}
