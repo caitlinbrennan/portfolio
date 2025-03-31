90aa771946e937f53423a0282ff57533805df059
Caitlin-Brennan <116352640+Caitlin-Brennan@users.noreply.github.com>
2025-03-31 22:37:04 +0100

    dark and light mode follow through pages when loaded

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,6 +32,16 @@ function openGame(){
 }
 
 function darkMode() {
-  var element = document.body;
-  element.classList.toggle("dark-mode");
+  document.body.classList.toggle('dark-mode');
+  if(document.body.classList.contains('dark-mode')) {
+    localStorage.setItem('theme', 'dark');
+  }
+  else {
+    localStorage.setItem('theme', 'light');
+  }
 }
+window.onload = function() {
+  if (localStorage.getItem('theme') === 'dark') {
+    document.body.classList.add('dark-mode');
+  }
+};
